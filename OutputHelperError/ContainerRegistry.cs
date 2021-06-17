ba54f1cb735ceaa833ea923055b8ff2625d2f7e4
szandiborbas <alexandra.borbas@instructure.com>
2021-06-17 12:06:50 +0200

    add specflow issue code

---
+++ OutputHelperError/ContainerRegistry.cs
@@ -0,0 +1,29 @@
+﻿using Autofac;
+using SpecFlow.Autofac;
+using System;
+using System.Linq;
+using TechTalk.SpecFlow;
+
+namespace OutputHelperError
+{
+    public class ContainerRegistry
+    {
+        //SpecFlow.Autofac: SpecFlow plugin for using Autofac as a dependency injection framework.
+        [ScenarioDependencies]
+        public static ContainerBuilder CreateContainerBuilder()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterTypes(CollectTypes())
+                   .PropertiesAutowired();
+
+            return builder;
+        }
+
+        private static Type[] CollectTypes() =>
+            AppDomain.CurrentDomain.GetAssemblies()
+                .SelectMany(a => a.GetTypes())
+                .Where(t => Attribute.IsDefined(t, typeof(BindingAttribute)))
+                .ToArray();
+    }
+}
---
+++ OutputHelperError/Hooks/TestTeardown.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Infrastructure;
+
+namespace OutputHelperError.Hooks
+{
+    [Binding]
+    public class TestTeardown
+    {
+        private readonly ISpecFlowOutputHelper _helper;
+
+        public TestTeardown(ISpecFlowOutputHelper helper)
+        {
+            _helper = helper;
+        }
+
+        [AfterScenario(Order = 0)]
+        public void Foo()
+        {
+        }
+    }
+}
